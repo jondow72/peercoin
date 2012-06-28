b4741115d59854b6989734bf4079a9798a1fd14b
Matt Corallo <matt@bluematt.me>
2012-06-28 15:52:45 +0200

    Revert "*Always* send a shutdown signal to enable custom shutdown actions"
    This reverts commit 896899e0d66e25f6549a92749d237c8a87b12f08.

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -860,7 +860,8 @@ void ThreadRPCServer2(void* parg)
     }
 
     vnThreadsRunning[THREAD_RPCLISTENER]--;
-    io_service.run();
+    while (!fShutdown)
+        io_service.run_one();
     vnThreadsRunning[THREAD_RPCLISTENER]++;
     StopRequests();
 }
