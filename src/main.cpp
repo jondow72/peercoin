54b46adecd48b8b004e19c44e3f476e6d610bf37
Matt Corallo <git@bluematt.me>
2012-08-20 21:10:25 -0400

    Let a node opt out of tx invs before we get a their bloom filter
    Note that the default value for fRelayTxes is false, meaning we
    now no longer relay tx inv messages before receiving the remote
    peer's version message.

--- src/main.cpp
+++ src/main.cpp
@@ -2907,6 +2907,10 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
             vRecv >> pfrom->strSubVer;
         if (!vRecv.empty())
             vRecv >> pfrom->nStartingHeight;
+        if (!vRecv.empty())
+            vRecv >> pfrom->fRelayTxes; // set to true after we get the first filter* message
+        else
+            pfrom->fRelayTxes = true;
 
         if (pfrom->fInbound && addrMe.IsRoutable())
         {
@@ -3501,6 +3505,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
             delete pfrom->pfilter;
             pfrom->pfilter = new CBloomFilter(filter);
         }
+        pfrom->fRelayTxes = true;
     }
 
 
@@ -3529,6 +3534,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         LOCK(pfrom->cs_filter);
         delete pfrom->pfilter;
         pfrom->pfilter = NULL;
+        pfrom->fRelayTxes = true;
     }
 
 
