e4348eaf25a4cc1dde722cfb132f361e36d18e56
Philip Kaufmann <phil.kaufmann@t-online.de>
2012-10-14 13:43:42 +0200

    fix some thread related log messages
    - "ThreadIRCSeed started" was not displayed, even if the thread ran
      (although only for a short time as the "do we want this thread?"-checks
      happen IN ThreadIRCSeed2())
    - the patch ensures we always get that message
    - add a "ThreadIRCSeed trying to connect..." message
    - add missing "ThreadDumpAddress started" message

--- src/net.cpp
+++ src/net.cpp
@@ -1240,6 +1240,8 @@ void DumpAddresses()
 
 void ThreadDumpAddress2(void* parg)
 {
+    printf("ThreadDumpAddress started\n");
+
     vnThreadsRunning[THREAD_DUMPADDRESS]++;
     while (!fShutdown)
     {
