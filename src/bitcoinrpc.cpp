d096bc726189f945f0278912a688c89d685bc617
Matt Corallo <git@bluematt.me>
2013-01-23 11:45:00 -0500

    Add addnode RPC command.

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -203,6 +203,7 @@ static const CRPCCommand vRPCCommands[] =
     { "getblockcount",          &getblockcount,          true,   false },
     { "getconnectioncount",     &getconnectioncount,     true,   false },
     { "getpeerinfo",            &getpeerinfo,            true,   false },
+    { "addnode",                &addnode,                true,   true },
     { "getdifficulty",          &getdifficulty,          true,   false },
     { "getdifficultym",         &getdifficultym,         true,   false },
     { "getnetworkhashps",       &getnetworkhashps,       true,   false },
