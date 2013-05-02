f660a575b4c76c99705f7c7b0790f96401af4d73
Gavin Andresen <gavinandresen@gmail.com>
2013-05-02 12:26:33 -0400

    Exit cleanly if AppInit2 returns false
    Bitcoin-Qt could core dump if application initialization failed in certain ways.
    
    I double-fixed this:
    1) qt/bitcoin.cpp now shuts down core threads cleanly if AppInit2 returns false
    2) init.cpp now exits before StartNode() if strErrors is set (no reason to StartNode if we're just going to exit immediately anyway).
    
    Tested by triggering all of the various ways AppInit2 can fail, either by passing bogus command-line arguments or just recompiling tweaked code to simulate failure.
    
    This is a partial fix for #2480

--- src/init.cpp
+++ src/init.cpp
@@ -987,6 +987,9 @@ bool AppInit2(boost::thread_group& threadGroup)
     if (!CheckDiskSpace())
         return false;
 
+    if (!strErrors.str().empty())
+        return InitError(strErrors.str());
+
     RandAddSeedPerfmon();
 
     //// debug print
@@ -1008,9 +1011,6 @@ bool AppInit2(boost::thread_group& threadGroup)
     // ********************************************************* Step 12: finished
 
     uiInterface.InitMessage(_("Done loading"));
-
-    if (!strErrors.str().empty())
-        return InitError(strErrors.str());
 
      // Add wallet transactions that aren't already in a block to mapTransactions
     pwalletMain->ReacceptWalletTransactions();
