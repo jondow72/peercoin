efe8d97f0e64fecd6d27b906eb51c74a842ec4db
Matt Whitlock <bitcoin@mattwhitlock.name>
2024-08-07 22:04:40 -0400

    add missing #include <cstdint> for GCC 15

--- src/node/interface_ui.h
+++ src/node/interface_ui.h
@@ -6,9 +6,10 @@
 #ifndef BITCOIN_NODE_INTERFACE_UI_H
 #define BITCOIN_NODE_INTERFACE_UI_H
 
+#include <cstdint>
 #include <functional>
-#include <memory>
 #include <string>
+#include <vector>
 #include <util/ui_change_type.h>
 
 class CBlockIndex;
