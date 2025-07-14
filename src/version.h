fdeda9975a7d172fa0f48803e2c9452bbbb5e553
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-07-14 20:10:24 +0200

    Update version.h

--- src/version.h
+++ src/version.h
@@ -9,17 +9,17 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70015;
-static const int OLD_VERSION = 70014; // peercoin: used to communicate with clients that don't know how to send PoS information in headers
+static const int PROTOCOL_VERSION = 71066;
+static const int OLD_VERSION = 71065; // peercoin: used to communicate with clients that don't know how to send PoS information in headers
 
 //! initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
+static const int INIT_PROTO_VERSION = 71064;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 31800;
+static const int GETHEADERS_VERSION = 71066;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
+static const int MIN_PEER_PROTO_VERSION = 71064;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -29,18 +29,18 @@ static const int CADDR_TIME_VERSION = 31402;
 static const int BIP0031_VERSION = 60000;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70011;
+static const int NO_BLOOM_VERSION = 71066;
 
 //! "sendheaders" command and announcing blocks with headers starts with this version
-static const int SENDHEADERS_VERSION = 70012;
+static const int SENDHEADERS_VERSION = 71066;
 
 //! "feefilter" tells peers to filter invs to you by fee starts with this version
-static const int FEEFILTER_VERSION = 70013;
+static const int FEEFILTER_VERSION = 71066;
 
 //! short-id-based block download starts with this version
-static const int SHORT_IDS_BLOCKS_VERSION = 70014;
+static const int SHORT_IDS_BLOCKS_VERSION = 71066;
 
 //! not banning for invalid compact blocks starts with this version
-static const int INVALID_CB_NO_BAN_VERSION = 70015;
+static const int INVALID_CB_NO_BAN_VERSION = 71066;
 
 #endif // BITCOIN_VERSION_H
