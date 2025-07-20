2148da099e4aed702966adf3a5ff2ec236df5674
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-07-20 20:11:15 +0200

    Update validation.h

--- src/validation.h
+++ src/validation.h
@@ -61,6 +61,47 @@ double GetPoSKernelPSV2(const CBlockIndex* blockindex = nullptr, int lookup = 72
 double GetPoSKernelPSV3(const CBlockIndex* blockindex = nullptr);
 
 const CBlockIndex* GetLastPoWBlockIndex(const CBlockIndex* pindex);
+int64_t GetTargetSpacingWork(int nHeight);
+double GetDifficultyFromBitsV2(const CBlockIndex* pindex0, bool fPrintInfo=false);
+
+
+
+
+static const int MAX_MAGI_POW_HEIGHT = 25000000;
+static const int PRM_MAGI_POW_HEIGHT = 80000;
+static const int PRM_MAGI_POW_HEIGHT_V2 = 50000; // re-cal PoW-I end block
+static const int END_MAGI_POW_HEIGHT = 500000;
+static const int END_MAGI_POW_HEIGHT_V2 = 5000000; // PoW-II aims to issue 12 mil and more than 10 years
+
+static const int BLOCK_REWARD_ADJT = 2700;
+static const int BLOCK_REWARD_ADJT_M7M_V2 = 32750;
+
+static const unsigned int MAX_BLOCK_SIZE = 1000000;
+static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
+static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
+static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
+static const unsigned int MAX_INV_SZ = 50000;
+// consensus/amount.h static const int64 COINS_BURNED = 720000 * COIN; // Notes: https://bitcointalk.org/index.php?topic=735170.msg9475622#msg9475622
+// consensus/amount.h static const int64 MIN_TX_FEE = .0001 * COIN;
+// consensus/amount.h static const int64 MIN_RELAY_TX_FEE = MIN_TX_FEE;
+// consensus/amount.h static const int64 MAX_MONEY = 25000000 * COIN + COINS_BURNED;  // NOte: the amount of COINS_BURNED is unspendable
+//static const int64 MAX_MONEY_POW_PRM = 10000000 * COIN;	// 10 mil; 5.5 mil in 1st magipow
+//static const int64 MAX_MONEY_POW_END = 15000000 * COIN;	// 15 mil; 5 mil in 2nd magipow
+static const double MAX_MAGI_PROOF_OF_STAKE = 0.05;		// dynamic annual interest, max 5%
+static const double MAX_MAGI_BALANCE_in_STAKE = 0.15;		// balance/money supply, max 15%
+static const int64 MAX_MONEY_STAKE_REF = 5000000 * COIN;	// 5 mil
+static const int64 MAX_MONEY_STAKE_REF_V2 = 500000 * COIN;	// 0.5 mil
+
+// consensus/amount.h static const int64 MIN_TXOUT_AMOUNT = MIN_TX_FEE;
+
+static const int nCoinbaseMaturity = 100;            // 100 blocks
+static const int nCoinbaseMaturityADJ = 500;            // 500 blocks
+
+inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
+// script/script.h // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
+// script/script.h static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
+
+
 
 
 
