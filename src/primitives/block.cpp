3bff47d222b1a8de6df7c7c53735788a5ca31e1c
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-07-20 14:34:55 +0200

    Update block.cpp M7M_v2

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -8,21 +8,19 @@
 #include <hash.h>
 #include <tinyformat.h>
 
-#include <../chainparams.h>
-#include "../crypto/m7m.h"
+#include <../util/system.h>
+#include <../crypto/m7m.h>
 
-// this hurts my brain and killed the rest of my braincells
-
-// bool fTestNet = Params().NetworkIDString() == CBaseChainParams::TESTNET;
-
-#define BEGIN(a)            ((char*)&(a))
-#define END(a)              ((char*)&((&(a))[1]))
+#ifndef BEGIN
+#define BEGIN(a) ((char*)&(a))
+#define END(a)   ((char*)&((&(a))[1]))
+#endif
 
 uint256 CBlockHeader::GetHash() const
 {
-    // if (fTestNet) {
-    // THIS SHOULD BE HASH_M7M_V2 !! UPDATE LATER
-        return hash_M7M(BEGIN(nVersion), END(nNonce));
+    bool fTestNet = gArgs.GetBoolArg("-testnet", false);
+    if (fTestNet) {
+        return hash_M7M_v2(BEGIN(nVersion), END(nNonce), nNonce);
         /*
         if(nTime < 1413590400) {
             return hash_M7M(BEGIN(nVersion), END(nNonce));
@@ -30,13 +28,13 @@ uint256 CBlockHeader::GetHash() const
             return hash_M7M_v2(BEGIN(nVersion), END(nNonce), nNonce);
         }
         */
-    // } else {
-    //     if(nTime < 1414330200) {
-    //         return hash_M7M(BEGIN(nVersion), END(nNonce));
-    //     } else {
-    //         return hash_M7M_v2(BEGIN(nVersion), END(nNonce), nNonce);
-    //     }
-    // }
+    } else {
+        if (nTime < 1414330200) {
+            return hash_M7M(BEGIN(nVersion), END(nNonce));
+        } else {
+            return hash_M7M_v2(BEGIN(nVersion), END(nNonce), nNonce);
+        }
+    }
 }
 
 std::string CBlock::ToString() const
