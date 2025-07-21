21c5405df389cb761d144a748f050d46954fadaa
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-07-21 23:41:46 +0200

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1596,7 +1596,7 @@ int64_t GetProofOfWorkRewardV2(const CBlockIndex* pindexPrev, int64_t nFees, boo
 int64_t GetProofOfWorkReward(unsigned int nBits, uint32_t nTime)
 {
 // Get the current block height
-    bool CheckFinalTxAtTip (int nBlockHeight, int nBlockTime)
+    bool CheckFinalTxAtTip (int nBlockHeight, int nBlockTime);
     int nHeight = nBlockHeight;
     bool fTestNet = gArgs.GetBoolArg("-testnet", false);
     double nDiff = GetDifficultyFromBits(nBits);
