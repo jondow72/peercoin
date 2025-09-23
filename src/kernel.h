381bd31f9dc7c451a24244b8bd3ba7d300a6236d
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-09-23 22:30:26 +0200

    Update kernel.h

--- src/kernel.h
+++ src/kernel.h
@@ -5,7 +5,7 @@
 #define PEERCOIN_KERNEL_H
 
 #include <primitives/transaction.h> // CTransaction(Ref)
-#include <../crypto/magimath.h>
+#include <validation.h>
 
 class CBlockIndex;
 class BlockValidationState;
