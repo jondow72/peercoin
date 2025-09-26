bfa7880893f859e15bcca19471181bc4113c3f01
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-09-26 17:18:21 +0200

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1717,7 +1717,7 @@ double GetAnnualInterestV2(int64_t nNetWorkWeit, double rMaxAPR, CBlockIndex* pi
 
 
 
-static const int64_t nTargetSpacingV3Work = 60 * 4;   // 4 min
+
 
 #define HEIGHT_DIFF_ADJ_TARGET_SPACKING_WORK_V3_INIT 1482000
 int64_t GetTargetSpacingWork(int nHeight)
