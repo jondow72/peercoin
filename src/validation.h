e96271df7f2c02d033463ddfb9ac4763a026f034
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-09-28 14:11:26 +0200

    Update validation.h

--- src/validation.h
+++ src/validation.h
@@ -1224,6 +1224,8 @@ bool DeploymentEnabled(const ChainstateManager& chainman, DEP dep)
 
 using FopenFn = std::function<FILE*(const fs::path&, const char*)>;
 
+// magi:
+bool fTestNetWeightV2 = false;
 // peercoin:
 CAmount GetProofOfWorkReward(unsigned int nBits, uint32_t nTime);
 CAmount GetProofOfStakeReward(int64_t nCoinAge, uint32_t nTime, uint64_t nMoneySupply);
