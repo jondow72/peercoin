2aaa4085c4a32dd22acc936d66a7869567ee2c50
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-09-28 22:03:53 +0200

    Update blockchain.h

--- src/rpc/blockchain.h
+++ src/rpc/blockchain.h
@@ -36,6 +36,10 @@ static constexpr int NUM_GETBLOCKSTATS_PERCENTILES = 5;
  */
 double GetDifficulty(const CBlockIndex* blockindex, const CBlockIndex* tip);
 
+extern double GetPoSKernelPS(const CBlockIndex* blockindex = NULL, int lookup = 72);
+extern double GetPoSKernelPSV2(const CBlockIndex* blockindex = NULL, int lookup = 72);
+extern double GetPoSKernelPSV3(const CBlockIndex* blockindex = NULL);
+
 /** Callback for when block tip changed. */
 void RPCNotifyBlockChange(const CBlockIndex*);
 
