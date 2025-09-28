ac6fdaf4b8435628c80d8b8b0d625d57f5937f1b
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-09-28 17:45:31 +0200

    Update validation.cpp include m7m.h magimath.h

--- src/validation.cpp
+++ src/validation.cpp
@@ -59,6 +59,9 @@
 #include <validationinterface.h>
 #include <warnings.h>
 
+#include <../crypto/m7m.h>  // For Magi M7M M7Mv2
+#include <../crypto/magimath.h>  // For mapBlockIndex and Magi constants
+
 #include <algorithm>
 #include <cassert>
 #include <chrono>
