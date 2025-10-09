d45710fd331c20c2064ef3d8f5272f53d8cb0404
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-10-09 20:38:48 +0200

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -4241,10 +4241,10 @@ bool CheckBlock(const CBlock& block, BlockValidationState& state, const Consensu
 
     // Check coinbase reward
     CAmount nCoinbaseCost = 0;
-    int64 nFees = 0;
+    CAmount nFees = 0;
     if (block.IsProofOfWork())
         int64 nPoWReward = (IsPoWIIRewardProtocolV2(block.nTime)) ? 
-			    GetProofOfWorkRewardV2(block, nFees, true) : 
+//			    GetProofOfWorkRewardV2(block, nFees, true) : 
 			    GetProofOfWorkReward(block.nBits, block->nHeight, nFees);
         nCoinbaseCost = (GetMinFee(*block.vtx[0], block.nTime) < PERKB_TX_FEE)? 0 : (GetMinFee(*block.vtx[0], block.nTime) - PERKB_TX_FEE);
   if (block.vtx[0]->GetValueOut() > (block.IsProofOfWork() ? ( nPoWReward - nCoinbaseCost ) : 0))
