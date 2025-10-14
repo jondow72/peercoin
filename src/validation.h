b859d43d3c913e17bfe6985a9d0c867e092f193c
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-10-14 21:39:31 +0200

    Update validation.h

--- src/validation.h
+++ src/validation.h
@@ -82,9 +82,9 @@ static const int nCoinbaseMaturityADJ = 500;            // 500 blocks
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
 // script/script.h static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 
-CBlockIndex* pindexBest = NULL;
+// CBlockIndex* pindexBest = NULL;
 
-bool fTestNetWeightV2 = false; // temp declared
+// bool fTestNetWeightV2 = false; // temp declared
 
 
 
