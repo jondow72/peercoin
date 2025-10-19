5a37fc137f95870cb2f2974dc558cde978a87c66
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-10-20 01:11:10 +0200

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -3722,9 +3722,9 @@ static bool CheckWitnessMalleation(const CBlock& block, bool expect_witness_comm
     return true;
 }
 
-bool CheckBlock(const CBlock& block, BlockValidationState& state, const Consensus::Params& consensusParams, bool fCheckPOW, bool fCheckMerkleRoot, bool fCheckSignature, int64 nHeight)
+bool CheckBlock(const CBlock& block, BlockValidationState& state, const Consensus::Params& consensusParams, bool fCheckPOW, bool fCheckMerkleRoot, bool fCheckSignature, int64_t nHeight)
 {
-    int64 nFees = 0;
+    int64_t nFees = 0;
     // These are checks that are independent of context.
 
     if (block.fChecked)
@@ -3789,7 +3789,7 @@ bool CheckBlock(const CBlock& block, BlockValidationState& state, const Consensu
     CAmount nCoinbaseCost = 0;
     if (block.IsProofOfWork())
         nCoinbaseCost = (GetMinFee(*block.vtx[0], block.nTime) < PERKB_TX_FEE)? 0 : (GetMinFee(*block.vtx[0], block.nTime) - PERKB_TX_FEE);
-        int64 nPoWReward = (IsPoWIIRewardProtocolV2(block.nTime)) ? 
+        int64_t nPoWReward = (IsPoWIIRewardProtocolV2(block.nTime)) ? 
 			    GetProofOfWorkRewardV2(nHeight, nFees, true) : 
 			    GetProofOfWorkReward(block.nBits, nHeight, nFees);
     if (block.vtx[0]->GetValueOut() > (block.IsProofOfWork()? (nPoWReward - nCoinbaseCost) : 0))
