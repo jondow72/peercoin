2e90cb07c41d9aabe9c0572c4868d769814ff2a3
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-10-23 22:06:03 +0200

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1703,7 +1703,7 @@ int64_t GetProofOfWorkRewardV2(const CBlockIndex* pindexPrev, int64_t nFees, boo
 }
 
 #define M7Mv2_SCALE 2.545
-int64_t GetProofOfWorkReward(int nBits, int nHeight, int64_t nFees)
+int64_t GetProofOfWorkReward(unsigned int nBits, int nHeight, int64_t nFees)
 {
     bool fTestNet = gArgs.GetBoolArg("-testnet", false);
     double nDiff = GetDifficultyFromBits(nBits);
