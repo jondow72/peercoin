f49f3602ef976bb21d02ffe495443c20e2574169
jondow72 <77569928+jondow72@users.noreply.github.com>
2026-06-13 14:37:08 +0200

    Update init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -15,7 +15,7 @@
 
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
-#include <boost/filesystem/convenience.hpp>
+#include <boost/filesystem/operations.hpp>
 #include <boost/interprocess/sync/file_lock.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <openssl/crypto.h>
