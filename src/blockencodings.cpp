6c888e35c219a30c72efa048698f4c52d177c71a
jondow72 <77569928+jondow72@users.noreply.github.com>
2026-06-19 00:21:06 +0200

    update 6

--- src/blockencodings.cpp
+++ src/blockencodings.cpp
@@ -207,7 +207,7 @@ ReadStatus PartiallyDownloadedBlock::FillBlock(CBlock& block, const std::vector<
 
     BlockValidationState state;
     CheckBlockFn check_block = m_check_block_mock ? m_check_block_mock : CheckBlock;
-    if (!check_block(block, state, Params().GetConsensus(), /*fCheckPoW=*/true, /*fCheckMerkleRoot=*/true, true)) {
+    if (!check_block(block, state, Params().GetConsensus(), /*fCheckPoW=*/true, /*fCheckMerkleRoot=*/true, true, /*nHeight=*/0)) {
         // TODO: We really want to just check merkle tree manually here,
         // but that is expensive, and CheckBlock caches a block's
         // "checked-status" (in the CBlock?). CBlock should be able to
