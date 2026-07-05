3592043d28d9ef80100a7d32f84bc528ae427ad5
jondow72 <77569928+jondow72@users.noreply.github.com>
2026-07-05 20:49:14 +0200

    Update validation.h

--- src/validation.h
+++ src/validation.h
@@ -44,6 +44,108 @@
 #include <thread>
 #include <utility>
 #include <vector>
+//----------------------------------------------------------------------------
+
+#include <../util/system.h> // For fTestNet
+
+extern CBlockIndex* pindexBest;
+// bool fTestNetWeightV2 = false; // temp declared
+extern bool fTestNetWeightV2;
+
+// magi:
+double GetAnnualInterestV2(int64_t nNetWorkWeit, double rMaxAPR);
+
+inline bool IsMiningProofOfWork(int nHeight)
+{
+    return nHeight <= MAX_MAGI_POW_HEIGHT;
+}
+//inline bool IsMiningProofOfWork() { return true; }
+
+inline bool IsMiningProofOfStake(int nHeight ) 
+{
+    bool fTestNet = gArgs.GetBoolArg("-testnet", false);
+    if (fTestNet) return nHeight > 10;
+    if (nHeight <= BLOCK_REWARD_ADJT) return (nHeight > 6720); // two weeks
+    else return (nHeight > 10080); // three weeks
+}
+
+//#define FORK_BLOCK_REWARDS_V2_TESNT 1419402600
+#define FORK_BLOCK_REWARDS_V2_TESNT 0
+#define FORK_BLOCK_REWARDS_V2 1420650000
+#define HEIGHT_CHAIN_SWITCH 1606950
+#define HEIGHT_PROTOCOL_V3 1825100
+
+inline bool IsPoWIIRewardProtocolV2(unsigned int nTime0)
+{
+    bool fTestNet = gArgs.GetBoolArg("-testnet", false);
+    if (fTestNet) {
+	   return (nTime0 > FORK_BLOCK_REWARDS_V2_TESNT);
+    } else {
+	   return (nTime0 > FORK_BLOCK_REWARDS_V2);
+    }
+}
+
+inline bool IsPoSIIProtocolV2(int nHeight)
+{
+    bool fTestNet = gArgs.GetBoolArg("-testnet", false);
+    if (fTestNet) {
+    	if (nHeight > 40860) fTestNetWeightV2 = true;
+	   else fTestNetWeightV2 = false;
+	   return nHeight > 40780;
+    } else return (nHeight > 131300);
+}
+
+inline bool IsProtocolV3(int nHeight)
+{
+    bool fTestNet = gArgs.GetBoolArg("-testnet", false);
+    if (fTestNet) return true;
+    return (nHeight > HEIGHT_PROTOCOL_V3);
+}
+
+inline unsigned int GetStakeMinAge(unsigned int nTime0) { return ( (nTime0 > 1503248400) ? (60 * 60 * 8) : (60 * 60 * 2) ); }
+
+inline int64_t GetMaxPoWWaitingTime()
+{
+    return (10 * 60); // Maximum time for PoW on hold
+}
+
+inline int64_t GetMaxPoSWaitingTime()
+{
+    return (3 * 60); // Maximum time for PoS on hold
+}
+
+
+
+inline bool IsChainRuleSwitchedOff(int nHeight) { return (nHeight > HEIGHT_CHAIN_SWITCH); }
+inline unsigned int GetStakeTargetSpacing(int nHeight) { return IsProtocolV3(nHeight) ? 96 : 90; }
+
+int64_t GetTargetSpacingWork(int nHeight);
+
+
+bool IsBlockInvalid(int nHeight0, int64_t nTime, bool fProofOfStake, const CBlockIndex* pindexPrev);
+bool IsProofOfWorkBlockInvalid(int nHeight0, int64_t nTime, bool fProofOfStake, const CBlockIndex* pindexPrev);
+bool IsProofOfStakeBlockInvalid(int nHeight0, int64_t nTime, bool fProofOfStake, const CBlockIndex* pindexPrev);
+
+
+//----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Chainstate;
 class CBlockTreeDB;
@@ -1224,8 +1326,6 @@ bool DeploymentEnabled(const ChainstateManager& chainman, DEP dep)
 
 using FopenFn = std::function<FILE*(const fs::path&, const char*)>;
 
-// magi:
-double GetAnnualInterestV2(int64_t nNetWorkWeit, double rMaxAPR);
 // peercoin:
 CAmount GetProofOfWorkReward(unsigned int nBits, uint32_t nTime);
 CAmount GetProofOfStakeReward(int64_t nCoinAge, uint32_t nTime, uint64_t nMoneySupply);
