4e30baacf0fdbc0cba248ceb0900d43656e3e0e1
jondow72 <77569928+jondow72@users.noreply.github.com>
2026-07-06 01:31:23 +0200

    Update tx_verify.cpp

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -169,6 +169,11 @@ int64_t GetTransactionSigOpCost(const CTransaction& tx, const CCoinsViewCache& i
 
 bool Consensus::CheckTxInputs(const CTransaction& tx, TxValidationState& state, const CCoinsViewCache& inputs, int nSpendHeight, CAmount& txfee, const Consensus::Params& params, unsigned int nTimeTx, uint64_t nMoneySupply)
 {
+    if (coin.nTime() < 1764975810) {   // Your 5 million block timestamp
+//        LogPrintf("Consensus::CheckTxInputs() : Bypassed PoS check for early block time=%" PRId64 "\n", coin.nTime());
+        return true;
+    }
+
     // are the actual inputs available?
     if (!inputs.HaveInputs(tx)) {
         return state.Invalid(TxValidationResult::TX_MISSING_INPUTS, "bad-txns-inputs-missingorspent",
