1236c29a118306ec1093ab51d459e641f85ef8a8
jondow72 <77569928+jondow72@users.noreply.github.com>
2026-07-05 21:08:43 +0200

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -147,7 +147,6 @@ static bool fDebugMagiPoS = false;
 // Get time weight
 int64_t GetMagiWeight(int64_t nValueIn, int64_t nIntervalBeginning, int64_t nIntervalEnd)
 {
-    bool fTestNet = gArgs.GetBoolArg("-testnet", false);
     double nWeight = 0;
     int64_t nnMoneySupply = MAX_MONEY_STAKE_REF;
 
@@ -158,8 +157,6 @@ int64_t GetMagiWeight(int64_t nValueIn, int64_t nIntervalBeginning, int64_t nInt
 
     if (rMro/6 >= MAX_MAGI_BALANCE_in_STAKE) return 0;
 
-    if (fTestNet) return GetMagiWeight_TestNet(nValueIn, nIntervalBeginning, nIntervalEnd);
-    
     nWeight = 5.55243 * ( pow(rEpf, -0.3 * rStakeDays * 4. / 8.177) - pow(rEpf, -0.6 * rStakeDays * 4. / 8.177) ) * rStakeDays;
 
     return max((int64_t)0, min((int64_t)(nWeight * 24 * 60 * 60), (int64_t)nStakeMaxAge));
@@ -168,7 +165,6 @@ int64_t GetMagiWeight(int64_t nValueIn, int64_t nIntervalBeginning, int64_t nInt
 // Get time weight
 int64_t GetMagiWeightV2(int64_t nValueIn, int64_t nIntervalBeginning, int64_t nIntervalEnd)
 {
-//    bool fTestNet = gArgs.GetBoolArg("-testnet", false);
     double nWeight = 0;
     int64_t nnMoneySupply = MAX_MONEY_STAKE_REF_V2;
 
@@ -179,8 +175,6 @@ int64_t GetMagiWeightV2(int64_t nValueIn, int64_t nIntervalBeginning, int64_t nI
 
     if (rMro/6 >= MAX_MAGI_BALANCE_in_STAKE) return 0;
 
-//    if (fTestNet & !fTestNetWeightV2) return GetMagiWeight_TestNet(nValueIn, nIntervalBeginning, nIntervalEnd);
-    
     nWeight = 42.2474 * ( pow(rEpf, -0.55 * (rStakeDays+2.) / 0.4719) - pow(rEpf, -0.6 * (rStakeDays+2.) / 0.4719) ) * rStakeDays;
 
     if (fDebugMagiPoS) LogPrintf("@GetMagiWeightV2 = %" PRId64 "\n", max((int64_t)0, min((int64_t)(nWeight * 24 * 60 * 60), (int64_t)nStakeMaxAge)));
