a40aa25ddf3ed8c11df08a34caca073a7abec678
jondow72 <77569928+jondow72@users.noreply.github.com>
2026-07-12 12:47:11 +0200

    Update blockchain.cpp

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -128,6 +128,7 @@ double GetDifficulty(const CBlockIndex* blockindex, const CBlockIndex* tip)
 }
 
 // Magi-specific functions
+int nBestHeight = -1;
 extern CBlockIndex* pindexBest;
 static bool fDebugMagi = false;
 double GetMagiDifficulty(const CBlockIndex* blockindex)
